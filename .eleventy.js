be4befcf31e38b322719187831f53b4fc46be9c5
Noah Friedman <noah@noahfriedman.com>
2026-03-03 11:00:29 -0600

    Upgrade CMS to multi-category collaborative structure

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
 
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByTag("posts");
+  });
+
   return {
     dir: {
       input: "src",
